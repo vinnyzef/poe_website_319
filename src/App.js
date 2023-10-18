ba66fda51e75400d09b44e2196fe32c4b74181ec
vinnyzef <72477005+vinnyzef@users.noreply.github.com>
2023-10-18 13:31:47 -0500

    merging our work

--- src/App.js
+++ src/App.js
@@ -1,77 +1,23 @@
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Creators from "./Creators";
+import "./App.css";
+import PoeNav from "./Nav";
+import Home from "./Home";
 
-import React from "react"
-import Creators from "./Creators"
-import { RouterProvider, createBrowserRouter } from "react-router-dom"
-import "./App.css"
-import PoeNav from "./Nav"
-import Home from "./Home"
 
-
-const router = createBrowserRouter([
-  {
-    element: <PoeNav />,
-    children: [
-      {
-        path: "/",
-        element: <Home />,
-      },
-      {
-        path: "/creators",
-        element: <Creators />,
-      },
-    ],
-    // element: <Footer />,
-  },
-])
 function App() {
   return (
     <div className="App">
-      <div className="content-wrapper">
-        <div className="header">
-          <div className="title">POE</div>
-          <div className="subtitle">A Rock Opera Musical</div>
-        </div>
-        <div className='button-container'>
-        <button className='tickets-button'>BUY TICKETS</button>
-        </div>
-
-        <div className="quote-container">
-          <div className="quote">“Mesmerizing,</div>
-          <div className="quote-text">This is a filler text where we will be putting in a quote or something.” -Someone</div>
-        </div>
-        
-        <div className="body">
-          <div className='box-container'>
-          <div className='box-content-card'>
-          <div className='box-content-card-image'>
-
-          </div>
-          <h3>Keep a peice of us with you</h3>
-          <p>Check out our latest apparel,
-accessories and more.</p>
-          </div>
-          <div className='box-content-card'>
-              <div className='box-content-card-image'>
-            
-          </div>
-          <h3>Keep a peice of us with you</h3>
-          <p>Check out our latest apparel,
-accessories and more.</p>
-          </div>
-          <div className='box-content-card'>
-          <div className='box-content-card-image'>
-            
-            </div>
-            <h3>Keep a peice of us with you</h3>
-            <p>Check out our latest apparel,
-accessories and more.</p>
-          </div>
-          </div>
-        </div>
-      </div>
+      <Router>
+        <PoeNav />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/creators" element={<Creators/>} />
+          </Routes>
+      </Router>
     </div>
-  )
+  );
 }
 
-export default Page(App);
-
+export default App;
--- src/Home.js
+++ src/Home.js
@@ -1,9 +1,45 @@
 import React from "react"
 import Footer from "./Footer"
-export default function () {
+import "./App.css";
+
+export default function Home () {
   return (
-    <div>
-      Main page
+    <div className="Home">
+        <div className="content-wrapper">
+          <div className="header">
+            <div className="title">POE</div>
+            <div className="subtitle">A Rock Opera Musical</div>
+          </div>
+          <div className='button-container'>
+            <button className='tickets-button'>BUY TICKETS</button>
+          </div>
+
+          <div className="quote-container">
+            <div className="quote">“Mesmerizing,</div>
+            <div className="quote-text">This is a filler text where we will be putting in a quote or something.” -Someone</div>
+          </div>
+          
+          <div className="body">
+            <div className='box-container'>
+              <div className='box-content-card'>
+                <div className='box-content-card-image'></div>
+                <h3>Keep a piece of us with you</h3>
+                <p>Check out our latest apparel, accessories, and more.</p>
+              </div>
+              <div className='box-content-card'>
+                <div className='box-content-card-image'></div>
+                <h3>Keep a piece of us with you</h3>
+                <p>Check out our latest apparel, accessories, and more.</p>
+              </div>
+              <div className='box-content-card'>
+                <div className='box-content-card-image'></div>
+                <h3>Keep a piece of us with you</h3>
+                <p>Check out our latest apparel, accessories, and more.</p>
+              </div>
+            </div>
+          </div>
+          </div>
+          <div/>
       <Footer />
     </div>
   )
